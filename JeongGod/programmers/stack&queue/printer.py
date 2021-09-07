00a5b61a72a48bada7661181303153873a0a1154
JeongGod <jeonggu223@gmail.com>
2021-09-08 00:04:58 +0900

    [2021.09.07] 이정규 Programmers 프린터, 다리를 지나는 트럭 lv2 - 프린터 lv2 - 다리를 지나는 트럭

---
+++ JeongGod/programmers/stack&queue/printer.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+def solution(priorities, location):
+    answer = 0
+    priorities
+    dq = deque(zip(range(len(priorities)), priorities))
+    prior_list = sorted(priorities)
+
+    while dq:
+        loc, prior = dq.popleft()
+
+        if prior_list[-1] > prior:
+            dq.append((loc, prior))
+            continue        
+        # 인쇄가능하다면
+        prior_list.pop()
+        answer += 1
+        if loc == location:
+            return answer
+        
+    return answer
