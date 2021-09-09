38458e42a5fc7eccdf4a39b45627d6549a88ea9e
JeongGod <jeonggu223@gmail.com>
2021-09-09 23:18:20 +0900

    [2021.09.09] 이정규 Programmers 가장 큰 수, 메뉴 리뉴얼 lv2 - 가장 큰 수 lv2 - 메뉴 리뉴얼

---
+++ JeongGod/programmers/kakao/menu.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+from itertools import combinations
+
+
+def solution(orders, course):
+    '''
+    가장 많이 함께 주문한 단품메뉴들을 코스요리 메뉴로 구 성
+    2가지 이상, 2명 이상의 주문의 단품메뉴 후보
+    '''
+    _dict = defaultdict(int)
+    # 손님이 시킨 메뉴에서 모든 조합을 알아본다.
+    for order in orders:
+        for n in course:
+            for i in combinations(sorted(list(order)), n):
+                _dict[i] += 1
+
+    results = sorted(_dict.items(), key=lambda x: x[1], reverse=True)
+    # "오름차순"으로 주어진다고 했으므로 course[-1]이 최댓값
+    course_dict = defaultdict(int)
+    for num in course:
+        # 최소 2명 이상의 손님에게 주문받은것만 가능
+        course_dict[num] = 2
+
+    answer = []
+    for menu, cnt in results:
+
+        # 먼저 course에 해당하는 개수인지 확인 => 현재 course에서 요구하는 최대메뉴가 맞는지 확인
+        if len(menu) in course_dict and cnt >= course_dict[len(menu)]:
+            course_dict[len(menu)] = cnt
+            answer.append(''.join(menu))
+    print(results) 
+    print(answer)
+    return sorted(answer)
